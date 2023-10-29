f0388b8e55a1e57a067350bb24e6d77958089463
xDAnkit <xDAnkitjain@gmail.com>
2023-10-29 13:38:55 +0530

    Part-6 ReactJS Setup

---
+++ Part-6 - ReactJS/src/features/Homepage/Homepage.Layout.jsx
@@ -0,0 +1,11 @@
+const Homepage = () => {
+  return (
+    <div>
+      <section className="banner">
+        <p>Banner Area</p>
+      </section>
+    </div>
+  );
+};
+
+export default Homepage;
---
+++ Part-6 - ReactJS/src/main.jsx
@@ -0,0 +1,5 @@
+import ReactDOM from "react-dom/client";
+import Homepage from "./features/Homepage/Homepage.Layout.jsx";
+import "./assets/sass/main.scss";
+
+ReactDOM.createRoot(document.getElementById("root")).render(<Homepage />);
